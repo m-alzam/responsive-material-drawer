3815369075b8c65c18ba2e2002ba4c4750ab01fa
Glitch (material-starter-kit) <none>
2018-12-20 20:35:10 +0000

    🎢🐓 Checkpoint ./app.js:2420423/1036 ./app.scss:2420423/1342 ./index.html:2420423/2974 ./package.json:2420423/1880

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-import {MDCRipple} from '@material/ripple/index';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {MDCDrawer} from "@material/drawer";
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const drawerElement = document.querySelector('.mdc-drawer');
+
+const initModalDrawer = () => {
+  drawerElement.classList.add("mdc-drawer--modal");
+  const drawer = MDCDrawer.attachTo(drawerElement);
+  drawer.open = false;
+  topAppBar.setScrollTarget(document.getElementById('main-content'));
+  topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+  });
+}
+if (window.matchMedia("(max-width: 900px)").matches) {
+  initModalDrawer();
+}
+
+const resizeHandler = () => {
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    initModalDrawer();
+  } else {
+    drawerElement.classList.remove("mdc-drawer--modal");
+  }
+}
+window.addEventListener('resize', resizeHandler);
