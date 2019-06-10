a32ca60ef212de0b2f57f8263d9a50676bb5760c
Glitch (material-starter-kit) <none>
2019-06-10 14:00:55 +0000

    🛰🐪 Checkpoint ./app.js:2116398/580

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ const mainContentEl = document.querySelector('.main-content');
 
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
+// Initialize drawer & list on demand
+
+let drawer;
+let list;
+
 // Initialize drawer
 
 const initModalDrawer = () => {
-  drawerElement.classList.add("mdc-drawer--modal");
-  const drawer = MDCDrawer.attachTo(drawerElement);
+  drawerElement.classList.add("mdc-drawer--modal");  
   drawer.open = false;
   topAppBar.setScrollTarget(mainContentEl);
   topAppBar.listen('MDCTopAppBar:nav', () => {
@@ -29,20 +33,24 @@ const initModalDrawer = () => {
   });
 }
 
-if (window.matchMedia("(max-width: 900px)").matches) {
+if (window.matchMedia("(max-width: 900px)").matches) {  
   initModalDrawer();
 } else {
-  const list = new MDCList(listEl);
+  list = new MDCList(listEl);
   list.wrapFocus = true;
 }
 
 // Toggle between permanent drawer and modal drawer at breakpoint 900px
 
 const resizeHandler = () => {
+  list.destroy();
+
   if (window.matchMedia("(max-width: 900px)").matches) {
     initModalDrawer();
   } else {
     drawerElement.classList.remove("mdc-drawer--modal");
+    list = new MDCList(listEl);
+    list.wrapFocus = true;
   }
 }
 window.addEventListener('resize', resizeHandler);
